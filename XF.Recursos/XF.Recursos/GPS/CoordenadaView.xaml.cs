e6ad1fbfdb3255a00ae56bf0c74142c439a38c46
Diego Leonardo Balduino <dbalduino.sup@conductoraz.onmicrosoft.com>
2017-10-30 20:57:07 -0200

    Add project files.

---
+++ XF.Recursos/XF.Recursos/GPS/CoordenadaView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Recursos.GPS
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoordenadaView : ContentPage
+    {
+        public CoordenadaView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCoordenada_Clicked(object sender, EventArgs e)
+        {
+            var localizacao = DependencyService.Get<ILocalizacao>();
+
+            localizacao.GetCoordenada();
+
+            MessagingCenter.Subscribe<ILocalizacao, Coordenada>(this, "coordenada", (loc, coordenada) =>
+               {
+                   lblLatitude.Text = coordenada.Latitude;
+                   lblLongitude.Text = coordenada.Longitude;
+               }
+            );
+        }
+    }
+}
